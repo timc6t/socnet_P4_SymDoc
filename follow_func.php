f1c832f51710b0f635ca5fb28d2f69eba6a6f479
Timothy Casiano <tim.casiano7@gmail.com>
2023-10-31 18:01:19 +0100

    Tercer commit: Edición del archivo follow_func.php

--- follow_func.php
+++ follow_func.php
@@ -1,30 +1,40 @@
 <?php
 // Code for user follows
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $follower_id = $_SESSION["user_id"];
-    $following_id = $_POST["following_id"]; // You can use a form input to specify the user to follow.
-
     // Validate and sanitize user input
-
-    // Check if the user is already following the target user
-    $check_query = "SELECT follow_id FROM follows WHERE follower_id = ? AND following_id = ?";
-    $stmt = $pdo->prepare($check_query);
-    $stmt->execute([$follower_id, $following_id]);
-
-    if ($stmt->rowCount() == 0) {
-        // User is not already following, so insert a follow record
-        $insert_query = "INSERT INTO follows (follower_id, following_id) VALUES (?, ?)";
-        $stmt = $pdo->prepare($insert_query);
-        $stmt->execute([$follower_id, $following_id]);
-
-        if ($stmt->rowCount() > 0) {
-            // Follow added successfully
-            // You can display a success message or redirect to the user's profile.
-        } else {
-            // Error handling: Follow insertion failed
-        }
+    $following_id = filter_var($_POST["following_id"], FILTER_SANITIZE_NUMBER_INT);
+    
+    // Ensure the following_id is not the same as the follower_id (you can adjust this validation as needed)
+    if ($following_id == $_SESSION["user_id"]) {
+        $error_message = "You can't follow yourself.";
     } else {
-        // User is already following, you can handle this case as needed.
+        // Check if the user is already following the target user
+        $check_query = "SELECT follow_id FROM follows WHERE follower_id = ? AND following_id = ?";
+        $stmt = $pdo->prepare($check_query);
+        $stmt->execute([$_SESSION["user_id"], $following_id]);
+
+        if ($stmt->rowCount() == 0) {
+            // User is not already following, so insert a follow record
+            $insert_query = "INSERT INTO follows (follower_id, following_id) VALUES (?, ?)";
+            $stmt = $pdo->prepare($insert_query);
+            $stmt->execute([$_SESSION["user_id"], $following_id]);
+
+            if ($stmt->rowCount() > 0) {
+                // Follow added successfully
+                $success_message = "You are now following this user.";
+            } else {
+                // Error handling: Follow insertion failed
+                $error_message = "Failed to add the follow. Please try again later.";
+            }
+        } else {
+            // User is already following
+            $error_message = "You are already following this user.";
+        }
     }
+    
+    // Redirect to the user's profile (you can adjust the URL as needed)
+    header("Location: profile.php?user_id=$following_id");
+    exit;
 }
+
 ?>
