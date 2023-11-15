284796bd30612084e1b631d97f26a479d5ef4a9e
Timothy Casiano <tim.casiano7@gmail.com>
2023-11-15 20:06:27 +0100

    Modificación de follow_func.php

--- follow_func.php
+++ follow_func.php
@@ -1,40 +1,74 @@
 <?php
-// Code for user follows
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate and sanitize user input
-    $following_id = filter_var($_POST["following_id"], FILTER_SANITIZE_NUMBER_INT);
-    
-    // Ensure the following_id is not the same as the follower_id (you can adjust this validation as needed)
-    if ($following_id == $_SESSION["user_id"]) {
-        $error_message = "You can't follow yourself.";
-    } else {
-        // Check if the user is already following the target user
-        $check_query = "SELECT follow_id FROM follows WHERE follower_id = ? AND following_id = ?";
-        $stmt = $pdo->prepare($check_query);
-        $stmt->execute([$_SESSION["user_id"], $following_id]);
+require_once "config.php";
+session_start();
 
-        if ($stmt->rowCount() == 0) {
-            // User is not already following, so insert a follow record
-            $insert_query = "INSERT INTO follows (follower_id, following_id) VALUES (?, ?)";
-            $stmt = $pdo->prepare($insert_query);
-            $stmt->execute([$_SESSION["user_id"], $following_id]);
-
-            if ($stmt->rowCount() > 0) {
-                // Follow added successfully
-                $success_message = "You are now following this user.";
-            } else {
-                // Error handling: Follow insertion failed
-                $error_message = "Failed to add the follow. Please try again later.";
-            }
-        } else {
-            // User is already following
-            $error_message = "You are already following this user.";
-        }
-    }
-    
-    // Redirect to the user's profile (you can adjust the URL as needed)
-    header("Location: profile.php?user_id=$following_id");
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
     exit;
 }
 
+$user_id = $_SESSION["user_id"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["follow_user_id"])) {
+        $follow_user_id = $_POST["follow_user_id"];
+
+        // Check if the user is trying to follow or unfollow
+        if (isset($_POST["follow"])) {
+            // Check if the user is not already following
+            if (!isFollowing($link, $user_id, $follow_user_id)) {
+                followUser($link, $user_id, $follow_user_id);
+                echo "Followed successfully!";
+            } else {
+                echo "You are already following this user.";
+            }
+        } elseif (isset($_POST["unfollow"])) {
+            // Check if the user is already following
+            if (isFollowing($link, $user_id, $follow_user_id)) {
+                unfollowUser($link, $user_id, $follow_user_id);
+                echo "Unfollowed successfully!";
+            } else {
+                echo "You are not following this user.";
+            }
+        }
+    } else {
+        echo "Invalid parameters.";
+    }
+} else {
+    echo "Invalid request.";
+}
+
+function isFollowing($link, $follower_id, $following_id) {
+    $sql = "SELECT * FROM follows WHERE follower_id = ? AND following_id = ?";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "ii", $follower_id, $following_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        $result = mysqli_stmt_num_rows($stmt) > 0;
+        mysqli_stmt_close($stmt);
+        return $result;
+    } else {
+        return false;
+    }
+}
+
+function followUser($link, $follower_id, $following_id) {
+    $sql = "INSERT INTO follows (follower_id, following_id) VALUES (?, ?)";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "ii", $follower_id, $following_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+}
+
+function unfollowUser($link, $follower_id, $following_id) {
+    $sql = "DELETE FROM follows WHERE follower_id = ? AND following_id = ?";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "ii", $follower_id, $following_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+}
+
+mysqli_close($link);
 ?>
